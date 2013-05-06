51f93918672d26a540afbc8f11f777b67bd21ff6
Windsdon <jpfm2210@gmail.com>
2013-05-06 18:16:55 -0300

    Springs!

--- main.cpp
+++ main.cpp
@@ -8,6 +8,7 @@
 #include <gl/gl.h>
 #include <gl/glu.h>
 #include <iostream>
+#include <math.h>
 
 #define WIDTH 1280
 #define HEIGHT 720
@@ -16,7 +17,6 @@ int main ( int argc, char** argv ) {
     freopen("CON", "w", stdout);
     freopen("CON", "w", stderr);
 
-    float px = 0, py = 0;
     float w = WIDTH;
     float h = HEIGHT;
 
@@ -33,6 +33,7 @@ int main ( int argc, char** argv ) {
         return 1;
     }
 
+
     glClearColor(0, 0, 0, 0);
     glClearDepth(1.0f);
 
@@ -47,26 +48,60 @@ int main ( int argc, char** argv ) {
 
     glEnable(GL_TEXTURE_2D);
 
-
-    while(1){
+    int running = 1;
+    int mousepos[2];
+    int numSlopes = 20;
+    double amplitude = 20;
+    double origin[] = {WIDTH/2, HEIGHT/2};
+    while(running) {
+        int i;
         SDL_Event e;
-        while(SDL_PollEvent(&e)){
-            if(e.type == SDL_KEYDOWN){
-                px += 10;
-                py += 10;
+        while(SDL_PollEvent(&e)) {
+            if(e.type == SDL_QUIT) {
+                running = 0;
+            }
+            if(e.type == SDL_MOUSEMOTION) {
+                SDL_GetMouseState(mousepos, mousepos+1);
             }
         }
+
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
         glLoadIdentity();
 
+//        glColor3f(1, 0, 0);
+//
+//        glBegin(GL_LINES);
+//        {
+//            glVertex2d(origin[0], origin[1]);
+//            glVertex2d(mousepos[0], mousepos[1]);
+//        }
+//        glEnd();
+
+
+        double angle = atan((mousepos[1] - origin[1])/(mousepos[0] - origin[0]))*180/3.141592;
+        int c = (mousepos[0] < origin[0])?1:-1;
+        //printf("Angle: %g\n", angle);
+        glTranslated(origin[0], origin[1], 0);
+        glRotated(angle, 0, 0, 1);
+        glScaled(c*sqrt(pow(origin[0] - mousepos[0], 2) + pow(origin[1] - mousepos[1], 2)), c, 1);
+        glTranslated(-origin[0], -origin[1], 0);
+
+
         glColor3f(1, 1, 1);
 
-        glBegin(GL_QUADS);
+        glBegin(GL_LINE_STRIP);
         {
-            glVertex2d(w/3.0 + px, h/3.0 + py);
-            glVertex2d(w/3.0 + px, 2*h/3.0 + py);
-            glVertex2d(2*w/3.0 + px, 2*h/3.0 + py);
-            glVertex2d(2*w/3.0 + px, h/3.0 + py);
+            //double factor = (amplitude/sqrt(pow((origin[1] - mousepos[1])/(mousepos[1] - origin[0]), 2) + 1));
+            //double vx = factor*(origin[1] - mousepos[1])/(mousepos[1] - origin[0]);
+            //double vy = factor;
+            int k = -1;
+            for(i = 0; i < 2*numSlopes + 1; i++) {
+                double px = -(i/(2.0*numSlopes));
+                //glVertex2d(origin[0] + px, origin[1] + amplitude);
+                //glVertex2d(origin[0] + px, origin[1] - amplitude);
+
+                glVertex2d(origin[0] + px, origin[1] + (!(i%2) ? 0 : -(k = -k))*amplitude);
+            }
         }
         glEnd();
 
